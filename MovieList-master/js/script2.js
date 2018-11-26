4aed077aedc388c66a9f8e7295a44f8ad4850dde
Foxsus <33105528+Foxsus@users.noreply.github.com>
2018-11-25 23:45:01 -0200

    Add files via upload

---
+++ MovieList-master/js/script2.js
@@ -0,0 +1,167 @@
+function detailRedeSocial() {
+    var text = document.getElementById("Movie");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailAOrigem() {
+    var text = document.getElementById("Movie1");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailArtist() {
+    var text = document.getElementById("Movie2");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme4() {
+    var text = document.getElementById("Movie4");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme5() {
+    var text = document.getElementById("Movie5");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme6() {
+    var text = document.getElementById("Movie6");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme7() {
+    var text = document.getElementById("Movie7");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme8() {
+    var text = document.getElementById("Movie8");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme9() {
+    var text = document.getElementById("Movie9");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme10() {
+    var text = document.getElementById("Movie10");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme11() {
+    var text = document.getElementById("Movie11");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme12() {
+    var text = document.getElementById("Movie12");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme13() {
+    var text = document.getElementById("Movie13");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme14() {
+    var text = document.getElementById("Movie14");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme15() {
+    var text = document.getElementById("Movie15");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme16() {
+    var text = document.getElementById("Movie16");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme17() {
+    var text = document.getElementById("Movie17");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme18() {
+    var text = document.getElementById("Movie18");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme19() {
+    var text = document.getElementById("Movie19");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme20() {
+    var text = document.getElementById("Movie20");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
+
+function detailFilme21() {
+    var text = document.getElementById("Movie21");
+    if (text.style.display === "none")
+        text.style.display = "block";
+    else
+        text.style.display = "none";
+}
